e813f3ac05807844c83995a5496f6c70e3c111d4
Kragown <kragown@gmail.com>
2025-11-25 14:16:20 +0100

    Initial Commit

---
+++ Fab/FabApp.swift
@@ -0,0 +1,17 @@
+//
+//  FabApp.swift
+//  Fab
+//
+//  Created by Nguyen Jean-Louis on 25/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct FabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
